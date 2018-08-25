25844849ac1a8e16dbb8a0329e321377b9cda9e3
Diego <dfosjgr@hotmail.com>
2018-08-24 22:06:43 -0600

    Hajimemashite, watashi ha Diego desu

---
+++ Persona/Persona/Persona.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    class Persona
+    {
+        public int edad { get; set; }
+        public string nombre { get; set; }
+        public string dni { get; set; }
+        public string sexo { get; set; }
+        public double peso { get; set; }
+        public double altura { get; set; }
+        public const string sexoD = "H";
+
+        public Persona()
+        {
+            this.nombre = "";
+            this.edad = 0;
+            this.sexo = sexoD;
+            this.peso = 0;
+            this.altura = 0;
+        }
+
+        public Persona(string nombre, int edad,string sexo)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+            this.peso = 0;
+            this.altura = 0;
+        }
+
+        public Persona(string nombre, int edad, string sexo, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+            this.peso = peso;
+            generarDNI();
+            this.altura = altura;
+            comprobarSexo();
+        }
+
+        private void comprobarSexo()
+        {
+            if (sexo != "H" && sexo != "M")
+            {
+                this.sexo = sexoD;
+            }
+        }
+
+        public int calcularIMC()
+        {
+            double pesoideal = peso / (Math.Pow(altura, 2));
+            if (pesoideal < 20)
+                return -1;
+            else if (pesoideal >= 20 && pesoideal <= 25)
+                return 0;
+            else
+                return 1;
+        }
+
+        public bool mayorEdad()
+        {
+            if (edad >= 18)
+                return true;
+            else
+                return false;
+        }
+
+        public string toString()
+        {
+            Console.WriteLine("Información de la persona");
+            return "Nombre: "+nombre + " Edad: " + edad + " Sexo: " + sexo + " DNI: " + dni + " Peso (Kg): " + peso + " Altura (M): " + altura;
+        }
+
+        private void generarDNI()
+        {
+            Random r = new Random();
+            int aleatorio = 99999999 - r.Next(1, 89999999);
+            char letra = Convert.ToChar(r.Next(65,90));
+            dni = Convert.ToString(aleatorio) + Convert.ToString(letra);
+        }
+    }
+}
---
+++ Persona/Persona/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Persona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca los datos de la persona 1");
+            Console.Write("Nombre: "); string n1 = Console.ReadLine();
+            Console.Write("Edad: "); int e1 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Sexo: "); string s1 = Console.ReadLine();
+            Console.Write("Peso (Kg): "); double p1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Altura (M): "); double a1 = Convert.ToDouble(Console.ReadLine());
+            Persona per1 = new Persona(n1, e1, s1, p1, a1);
+            comprobarIMC((per1.calcularIMC()));
+            Console.WriteLine(per1.toString());
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            Console.WriteLine("Introduzca los datos de la persona 2");
+            Console.Write("Nombre: "); string n2 = Console.ReadLine();
+            Console.Write("Edad: "); int e2 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Sexo: "); string s2 = Console.ReadLine();
+            Persona per2 = new Persona(n2, e2, s2);
+            Console.WriteLine(per2.toString());
+            Thread.Sleep(3000);
+
+
+            Console.ReadKey();
+        }
+
+        public static void comprobarIMC(int peso)
+        {
+            switch (peso)
+            {
+                case -1:
+                    Console.WriteLine("El peso de la persona está por debajo de lo ideal");
+                    break;
+                case 0:
+                    Console.WriteLine("La persona tiene un peso ideal");
+                    break;
+                case 1:
+                    Console.WriteLine("La persona tiene sobrepeso");
+                    break;
+                default:
+                    Console.WriteLine("Peso invalido");
+                    break;
+            }
+        }
+    }
+}
